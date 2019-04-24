32b5e1e8ba1d30ce9164d5d6a139b9dec7528c35
Liam Harbec <liamharbec@Jenkins-II.local>
2019-04-24 13:53:32 -0700

    Added functionality to add files to page. (No save functionality yet)

--- app/controllers/power_consumption_entries_controller.rb
+++ app/controllers/power_consumption_entries_controller.rb
@@ -1,10 +1,15 @@
 class PowerConsumptionEntriesController < ApplicationController
-  def import
-    PowerConsumptionEntry.import(params[:file])
-    redirect_to root_url, notice "Data imported."
-  end
+
 
   def create
+    # byebug
+
+    redirect_to power_consumption_entries_path, notice: "Upload Successfull"
+  end
+
+  def import
+    PowerConsumptionEntry.import(params[:file])
+    # redirect_to root_url, notice "Data imported."
   end
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
-  resources :power_consumption_entries
+  resources :power_consumption_entries, only: [:index, :create]
 
   root 'welcome#index'
 end
